1fa797bc88c7163fa3899b90acb96595b2334762
Jani Muhlestein <janimuhlestein@gmail.com>
2020-10-05 04:39:51 -0600

    changed route places

--- server.js
+++ server.js
@@ -99,16 +99,16 @@ app.post('/api/animals', (req,res) => {
     }
 });
 
-app.get('/', (req,res)=> {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
 app.get('/animals', (req,res)=>{
     res.sendFile(path.join(__dirname, './public/animals.html'));
 });
 
 app.get('/zookeepers', (req,res) => {
     res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('/', (req,res)=> {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.listen(PORT, () => {
